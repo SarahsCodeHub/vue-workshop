7b4bfab62f4a747c38757e4afc3baa333677800b
SarahsCodeHub <sa.michaelsen@gmail.com>
2020-03-05 16:18:02 +0100

    split into components

---
+++ src/components/Cli-Setup.vue
@@ -0,0 +1,30 @@
+<template>
+  <div>
+    <h3>Installed CLI Plugins</h3>
+    <ul>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
+          target="_blank"
+          rel="noopener"
+        >babel</a>
+      </li>
+      <li>
+        <a
+          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint"
+          target="_blank"
+          rel="noopener"
+        >eslint</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "cli-setup"
+};
+</script>
+
+<style>
+</style>
