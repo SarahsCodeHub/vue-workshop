e92c3835672e322160e23c6eecabe75b7a8762e5
SarahsCodeHub <sa.michaelsen@gmail.com>
2020-03-05 16:46:26 +0100

    add message to app data

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,17 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <hello-world msg="Welcome to Your Vue.js App" />
+    <h1>{{ message }}</h1>
   </div>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
-
 export default {
   name: "App",
-  components: {
-    HelloWorld
+  data() {
+    return {
+      message: "Hello World"
+    };
   }
 };
 </script>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>{{ message }} from our component</h1>
   </div>
 </template>
 
@@ -8,7 +8,7 @@
 export default {
   name: "hello-world",
   props: {
-    msg: String
+    message: String
   }
 };
 </script>
